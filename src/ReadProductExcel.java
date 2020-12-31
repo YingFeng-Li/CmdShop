f0cdca2e56ed6a16cb55ff2f0b5dd1c9f357937e
YingFeng-Li <153774157@qq.com>
2020-12-31 19:41:10 +0800

    登录成功，查询商品，加入购物车

--- src/ReadProductExcel.java
+++ src/ReadProductExcel.java
@@ -4,25 +4,23 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DecimalFormat;
 
 public class ReadProductExcel {
-    public static void main(String[] args) throws ClassNotFoundException {
-        InputStream in = Class.forName("Test").getResourceAsStream("Product.xlsx");
-        Product[] products = new ReadProductExcel().readExcel(in);
-
-    }
-        public Product[] readExcel(InputStream in) {
-            Product products[] = null;
-        try{
+    /*
+    readExcel是什么方法？成员方法
+     */
+    public Product getProductById(String id,InputStream in) {
+        try {
             XSSFWorkbook xw = new XSSFWorkbook(in);
             XSSFSheet xs = xw.getSheetAt(0);
-            products = new Product[xs.getLastRowNum()];
             for (int j = 1; j <= xs.getLastRowNum(); j++) {
                 XSSFRow row = xs.getRow(j);
-                Product  product= new Product();
+                Product product = new Product();
                 for (int k = 0; k <= row.getLastCellNum(); k++) {
                     XSSFCell cell = row.getCell(k);
                     if (cell == null)
@@ -36,20 +34,50 @@ public class ReadProductExcel {
                     } else if (k == 3) {
                         product.setDesc(this.getValue(cell));
                     }
-                    products[j - 1] = product;
-
+                }
+                if (id.equals(product.getId())){
+                    return product;
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-            return products;
+        return null;
+    }
+    public Product[] getAllProductExcel(InputStream inPro) {
+        Product products[] = null;
+        try {
+            XSSFWorkbook xw = new XSSFWorkbook(inPro);
+            XSSFSheet xs = xw.getSheetAt(0);
+            products = new Product[xs.getLastRowNum()];
+            for (int j = 1; j <= xs.getLastRowNum(); j++) {
+                XSSFRow row = xs.getRow(j);
+                Product product = new Product();
+                for (int k = 0; k <= row.getLastCellNum(); k++) {
+                    XSSFCell cell = row.getCell(k);
+                    if (cell == null)
+                        continue;
+                    if (k == 0) {
+                        product.setId(this.getValue(cell));
+                    } else if (k == 1) {
+                        product.setName(this.getValue(cell));
+                    } else if (k == 2) {
+                        product.setPrice(Float.valueOf(this.getValue(cell)));
+                    } else if (k == 3) {
+                        product.setDesc(this.getValue(cell));
+                    }
+                    products[j-1]=product;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
+        return products;
+    }
     private String getValue(XSSFCell cell) {
         String value;
         CellType type = cell.getCellTypeEnum();
-
+        DecimalFormat df=new DecimalFormat("#");
         switch (type) {
             case STRING:
                 value = cell.getStringCellValue();
@@ -61,7 +89,7 @@ public class ReadProductExcel {
                 value = cell.getBooleanCellValue() + "";
                 break;
             case NUMERIC:
-                value = cell.getNumericCellValue() + "";
+                value = df.format(cell.getNumericCellValue());
                 break;
             case FORMULA:
                 value = cell.getCellFormula();
--- src/Test.java
+++ src/Test.java
@@ -1,34 +1,71 @@
-import java.io.File;
+import java.io.InputStream;
 import java.util.Scanner;
+import java.io.File;
 
 public class Test {
-    public static void main(String[] args) {
-        /*
-        开始读文件
-         */
-        File file = new File("E:\\蓝桥学习\\MyCode\\CmdShop\\src\\users.xlsx");//一定要具体到某一个文件名称，而不是目录而已
-        ReadExcel readExcel = new ReadExcel();//创建对象  ReadExcel为一个类
-        User users[] = readExcel.readExcel(file);
 
-        boolean flag = true;
-        while(flag){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("请输入你的名字：");
-            String username = sc.next();
-            System.out.println("请输入你的密码：");
-            String password = sc.next();
+    public static void main(String[] args) throws ClassNotFoundException {
+        boolean bo=true;
+        while (bo){
+            System.out.println("请输入用户名:");
 
-            for (int i = 0; i < users.length; i++) ;
-            {
-                int i = 0;
-                if (username.equals(users[i].getName()) && password.equals(users[i].getPassword())) {
-                    System.out.println("登陆成功!");
-                    flag = false;
+            Scanner sc=new Scanner(System.in);
+            String uername=sc.next();
+
+
+            System.out.println("请输入你的密码:");
+            String password=sc.next();
+
+            InputStream inPro=Class.forName("Test").getResourceAsStream("product.xlsx");
+            InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
+            //File file=new File("C:\\Users\\151509\\IdeaProjects\\java\\src\\users.xlsx");
+            ReadExcel readExcel=new ReadExcel();
+            User users[]=readExcel.readExcel(in);
+            for (int i=0; i<users.length;i++) {
+                if (uername.equals(users[i].getName()) && password.equals(users[i].getPassword())) {
+                    System.out.println("登陆成功");
+                    ReadProductExcel readProductExcel=new ReadProductExcel();
+                    Product products[] =readProductExcel.getAllProductExcel(inPro);
+                    for (Product product:products) {
+                        System.out.print(product.getId());
+                        System.out.print("\t" + product.getName());
+                        System.out.print("\t" + product.getPrice());
+                        System.out.println("\t" + product.getDesc());
+                    }
+                    int count =0;
+                    Product products1[]=new Product[5];
+                    System.out.println("请输入商品ID");
+                    String pId=sc.next();
+                    ReadProductExcel readProductExcel1=new ReadProductExcel();
+                    inPro=null;
+                    inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
+                    Product product=readProductExcel1.getProductById(pId,inPro);
+                    if(product!=null){
+                        products1[count++]=product;//把商品加入购物车
+                        System.out.println("找到了该商品");
+                        System.out.println("你购买的商品数量为"+count);
+
+                    }
+                    System.out.println("你购物车里的商品为：");
+                    for(i=0;i<count;i++){
+
+                        System.out.print(+products1[i]);
+                    }
+                    System.out.println("是否继续购物");
+                    for (Product product1:products) {
+                        System.out.print(product.getId());
+                        System.out.print("\t" + product.getName());
+                        System.out.print("\t" + product.getPrice());
+                        System.out.println("\t" + product.getDesc());
+                    }
+
+                    bo=false;
+                    break;
                 } else {
-                    System.out.println("登录失败!请重新输入");
+                    System.out.println("登陆失败");
+                    System.out.println("正确的密码是" + users[i].getPassword());
                 }
             }
         }
-
     }
 }
